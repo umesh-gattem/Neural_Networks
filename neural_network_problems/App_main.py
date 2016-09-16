b795e8f44ae0123fb91f5c95b728255b6364381d
umesh <umesh.gattem@gmail.com>
2016-09-16 20:58:13 +0530

    Added App_main and network_model programs with fullt completed network

--- neural_network_problems/App_main.py
+++ neural_network_problems/App_main.py
@@ -7,22 +7,71 @@ defines the input and output for oyr network model.
 import pandas as pd
 import numpy as np
 from neural_network_problems import network_model
+from collections import OrderedDict
+
+# from neural_network_problems.network_model import train_network
 
 read_file = pd.read_csv("/home/umesh/mycsv_file.csv")
-train_data_input = []
-train_data_output = []
-for index, row in read_file.iterrows():
-    input = np.array([row[:-1]])
-    output = np.array([[row[len(row)-1]]])
-    train_data_input.append(input)
-    train_data_output.append(output)
+train_data_input, train_data_output = network_model.get_input_output(read_file)
 
-input_nodes = len(row[:-1])
-output_nodes = 1
-layers = [input_nodes, 6, 8, 9, output_nodes]
-epochs = 8000
+input_nodes = train_data_input[1].size
+output_nodes = train_data_output[1].size
+first_hidden_layer_nodes = 6
+second_hidden_layer_nodes = 8
+third_hidden_layer_nodes = 9
+layers_list = [input_nodes, first_hidden_layer_nodes, second_hidden_layer_nodes,
+               third_hidden_layer_nodes, output_nodes]
+epochs = 10000
 learning_rate = 0.9
 
-network = network_model.NetworkModel(layers, train_data_input, train_data_output, 10000, learning_rate)
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
 
 
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+
+np.random.seed(2)
+
+weight_dictionary = OrderedDict()
+
+weight_dictionary['weight1'] = np.random.randn(layers_list[0], layers_list[1])
+weight_dictionary['weight2'] = np.random.randn(layers_list[1], layers_list[2])
+weight_dictionary['weight3'] = np.random.randn(layers_list[2], layers_list[3])
+weight_dictionary['weight4'] = np.random.randn(layers_list[3], layers_list[4])
+
+bias_dictionary = OrderedDict()
+bias_dictionary['bias1'] = np.random.randn(1, layers_list[1])
+bias_dictionary['bias2'] = np.random.randn(1, layers_list[2])
+bias_dictionary['bias3'] = np.random.randn(1, layers_list[3])
+bias_dictionary['bias4'] = np.random.randn(1, layers_list[4])
+
+
+def define_model(activation, weights_list, bias_list):
+    activation_list = [activation]
+    sigmoid_list = []
+    z1 = np.add(np.dot(activation, weights_list[0]), bias_list[0])
+    sigmoid_list.append(z1)
+    activation1 = sigmoid(z1)
+    activation_list.append(activation1)
+    z2 = np.add(np.dot(activation1, weights_list[1]), bias_list[1])
+    sigmoid_list.append(z2)
+    activation2 = sigmoid(z2)
+    activation_list.append(activation2)
+    z3 = np.add(np.dot(activation2, weights_list[2]), bias_list[2])
+    sigmoid_list.append(z3)
+    activation3 = sigmoid(z3)
+    activation_list.append(activation3)
+    z4 = np.add(np.dot(activation3, weights_list[3]), bias_list[3])
+    sigmoid_list.append(z4)
+    activation4 = sigmoid(z4)
+    activation_list.append(activation4)
+    return activation_list, sigmoid_list
+
+
+weight_list = [weight for weight in weight_dictionary.values()]
+bias_list = [bias for bias in bias_dictionary.values()]
+network_model.NetworkModel(layers_list, train_data_input,
+                           train_data_output, epochs, learning_rate, weight_list, bias_list)
--- neural_network_problems/network_model.py
+++ neural_network_problems/network_model.py
@@ -3,7 +3,6 @@ This python file gives the dynamic network model for any problem.
  @since 15-09-2016
 """
 import numpy as np
-from neural_network_problems import train_data
 
 
 def sigmoid(z):
@@ -14,106 +13,78 @@ def sigmoid_prime(z):
     return sigmoid(z) * (1 - sigmoid(z))
 
 
+def get_input_output(csv_file):
+    train_data_input = []
+    train_data_output = []
+    for index, row in csv_file.iterrows():
+        input_data = np.array([row[:-1]])
+        output_data = np.array([[row[len(row) - 1]]])
+        train_data_input.append(input_data)
+        train_data_output.append(output_data)
+    return train_data_input, train_data_output
 
 
 class NetworkModel(object):
-    def __init__(self, layers, input, output, epochs, learning_rate):
+    def __init__(self, layers, input, output, epochs, learning_rate, weight_list, bias_list):
         self.train_data_input = input
         self.train_data_output = output
         self.layers = layers
         self.layers_size = len(layers)
-        self.bias = self.define_bias(layers)
-        print(self.bias[0].shape)
-        # exit()
-        self.weights = self.define_weights(layers)
-        # print(self.weights)
-        # print(self.bias)
-        # exit()
-        self.train_network(epochs, learning_rate, self.train_data_input, self.train_data_output)
+        self.train_network(layers, epochs, learning_rate, self.train_data_input,
+                           self.train_data_output, weight_list, bias_list)
 
-
-        # print(self.train_data_input)
-        # print(self.train_data_output)
-
-    def define_weights(self, layers):
-        weight_list = []
-        self.weight1 = np.random.randn(layers[0], layers[1])
-        weight_list.append(self.weight1)
-        self.weight2 = np.random.randn(layers[1], layers[2])
-        weight_list.append(self.weight2)
-        # print(self.weight2)
-        self.weight3 = np.random.randn(layers[2], layers[3])
-        weight_list.append(self.weight3)
-        self.weight4 = np.random.randn(layers[3], layers[4])
-        weight_list.append(self.weight4)
-        return weight_list
-
-    def define_bias(self, layers):
-        bias_list = []
-        self.bias1 = np.random.randn(1, layers[1])
-        bias_list.append(self.bias1)
-        self.bias2 = np.random.randn(1, layers[2])
-        bias_list.append(self.bias2)
-        self.bias3 = np.random.randn(1, layers[3])
-        bias_list.append(self.bias3)
-        self.bias4 = np.random.randn(1, layers[4])
-        bias_list.append(self.bias4)
-        return bias_list
-
-    def train_network(self, epochs, learning_rate, train_data_input, train_data_output):
+    def train_network(self, layers, epochs, learning_rate, train_data_input, train_data_output, weight_list, bias_list):
         for each_epoch in range(epochs):
-            # print(self.bias)
-            change_in_bias = [np.zeros(bias.shape) for bias in self.bias]
-            change_in_weights = [np.zeros(weights.shape) for weights in self.weights]
+            change_in_bias = [np.zeros(bias.shape) for bias in weight_list]
+            change_in_weights = [np.zeros(weights.shape) for weights in bias_list]
             for input, output in zip(train_data_input, train_data_output):
-                delta_weights, delta_bias = train_data.algo_to_train_data(self.layers, input, output, self.weights, self.bias)
-                # print(delta_bias)
-                # print(delta_weights)
+                from neural_network_problems.App_main import define_model
+                activation_list, sigmoid_list = define_model(input, weight_list, bias_list)
+                delta_weights, delta_bias = self.back_propagation(layers, activation_list,
+                                                                  output, weight_list, bias_list,
+                                                                  sigmoid_list)
                 change_in_bias = [(bias + d_bias) for bias, d_bias in zip(change_in_bias, delta_bias)]
                 change_in_weights = [(weights + d_weights) for weights, d_weights in zip(change_in_weights,
                                                                                          delta_weights)]
-            self.weights = [w - (learning_rate / len(train_data_input)) * nw for w, nw in
-                                zip(self.weights, change_in_weights)]
-            self.bias = [b - (learning_rate / len(train_data_input)) * nb for b, nb in zip(self.bias, change_in_bias)]
-            #     # print(change_in_weights)
-            #     # print(change_in_weights[-1])
-            #     # exit()
-            # for w, nw in zip(self.weight1, change_in_weights[0]):
-            #      self.weight1 = w - (learning_rate / len(train_data_input)) * nw
-            # self.weight2 = [(w - (learning_rate / len(train_data_input)) * nw) for w, nw in
-            #                 zip(self.weight2, change_in_weights[1])]
-            # print(self.weight2)
-            # exit()
-            # self.weight3 = [w - (learning_rate / len(train_data_input)) * nw for w, nw in
-            #                 zip(self.weight3, change_in_weights[2])]
-            # self.weight4 = [w - (learning_rate / len(train_data_input)) * nw for w, nw in
-            #                 zip(self.weight4, change_in_weights[3])]
-            # self.weights[0] = self.weight1
-            # self.weights[1] = self.weight2
-            # self.weights[2] = self.weight3
-            # self.weights[3] = self.weight4
-            # self.bias1 = [b - (learning_rate / len(train_data_input)) * nb for b, nb in
-            #               zip(self.bias1, change_in_bias[0])]
-            # self.bias2 = [b - (learning_rate / len(train_data_input)) * nb for b, nb in
-            #              zip(self.bias2, change_in_bias[1])]
-            # self.bias3 = [b - (learning_rate / len(train_data_input)) * nb for b, nb in
-            #              zip(self.bias3, change_in_bias[2])]
-            # self.bias4 = [b - (learning_rate / len(train_data_input)) * nb for b, nb in
-            #              zip(self.bias4, change_in_bias[3])]
-            # self.bias[0] = self.bias1
-            # self.bias[1] = self.bias2
-            # self.bias[2] = self.bias3
-            # self.bias[3] = self.bias4
-            # print(self.bias)
-            # # for bias in self.bias:
-            # #     print(bias.shape)
-            # # exit()
-
+            weight_list[0] = np.array([(w - (learning_rate / len(train_data_input)) * nw) for w, nw in
+                                       zip(weight_list[0], change_in_weights[0])])
+            weight_list[1] = np.array([(w - (learning_rate / len(train_data_input)) * nw) for w, nw in
+                                       zip(weight_list[1], change_in_weights[1])])
+            weight_list[2] = np.array([(w - (learning_rate / len(train_data_input)) * nw) for w, nw in
+                                       zip(weight_list[2], change_in_weights[2])])
+            weight_list[3] = np.array([(w - (learning_rate / len(train_data_input)) * nw) for w, nw in
+                                       zip(weight_list[3], change_in_weights[3])])
+            bias_list[0] = np.array([(b - (learning_rate / len(train_data_input)) * nb) for b, nb in
+                                     zip(bias_list[0], change_in_bias[0])])
+            bias_list[1] = np.array([(b - (learning_rate / len(train_data_input)) * nb) for b, nb in
+                                     zip(bias_list[1], change_in_bias[1])])
+            bias_list[2] = np.array([(b - (learning_rate / len(train_data_input)) * nb) for b, nb in
+                                     zip(bias_list[2], change_in_bias[2])])
+            bias_list[3] = np.array([(b - (learning_rate / len(train_data_input)) * nb) for b, nb in
+                                     zip(bias_list[3], change_in_bias[3])])
         for input in train_data_input:
-            self.test_data(input)
+            self.test_data(input, weight_list, bias_list)
 
-    def test_data(self, activation):
-        for w, b in zip(self.weights, self.bias):
+    def back_propagation(self, layers, activation_list, output, weights, bias, sigmoid_list):
+        change_in_bias = [np.zeros(b.shape) for b in bias]
+        change_in_weights = [np.zeros(w.shape) for w in weights]
+        z = sigmoid_list[-1]
+        delta = (self.cost_derivative(activation_list[-1], output)) * sigmoid_prime(z)
+        change_in_bias[-1] = delta
+        change_in_weights[-1] = np.dot(activation_list[-2].T, delta)
+        for layers in range(2, len(layers)):
+            z = sigmoid_list[-layers]
+            delta = (np.dot(delta, weights[-layers + 1].T)) * (sigmoid_prime(z))
+            change_in_bias[-layers] = delta
+            change_in_weights[-layers] = np.dot(activation_list[-layers - 1].T, delta)
+        return change_in_weights, change_in_bias
+
+    def cost_derivative(self, activation, output):
+        cost = (activation - output)
+        return cost
+
+    def test_data(self, activation, weight_list, bias_list):
+        for w, b in zip(weight_list, bias_list):
             z = np.add(np.dot(activation, w), b)
             activation = sigmoid(z)
         print(activation)
